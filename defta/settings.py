a95955574851dadcdc44dc2b2cd43c48837c0efc
Richard Kefa mcharo <59644856+richardkefa@users.noreply.github.com>
2021-06-01 16:33:58 +0300

    Update settings.py
    deleted exposed secret key

--- defta/settings.py
+++ defta/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '0(nis5w*um)x(2r+^_e#g91=w2)44#@ottfm&wz%$+%$zap7(o'
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
